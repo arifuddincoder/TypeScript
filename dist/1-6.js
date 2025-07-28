49b72a1d9db1ce87e0458b8e96e28e8098ac850e
arif128551 <xossarif@gmail.com>
2025-07-28 10:03:03 +0600

    done few tutorials

---
+++ dist/1-6.js
@@ -0,0 +1,10 @@
+"use strict";
+// learning functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+// console.log(add(6, 7));
+const addition = (a, b) => {
+    return a + b;
+};
+console.log(addition(7, 8));
