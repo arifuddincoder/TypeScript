8b8fd37381b81ec07da2451810ae18244767ce56
Md Arif Uddin <xossarif@gmail.com>
2025-08-25 18:26:41 +0600

    Update c3.ts

--- src/module-3/c3.ts
+++ src/module-3/c3.ts
@@ -1,2 +1,14 @@
 {
+  type common = string | number;
+
+const concatTwoItem = (param1: common, param2: common): common => {
+	if (typeof param1 === "number" && typeof param2 === "number") {
+		return param1 + param2;
+	} else {
+		return param1.toString() + param2.toString();
+	}
+};
+
+console.log(concatTwoItem(2, 3));
+console.log(concatTwoItem("2", "3"));
 }
