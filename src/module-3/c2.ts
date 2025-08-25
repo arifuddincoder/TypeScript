38aaca607c31d8c0d08a9a199b98657c6707ca7d
arifuddincoder <xossarif@gmail.com>
2025-08-25 17:29:09 +0600

    added answer of m-3 c-2

---
+++ src/module-3/c2.ts
@@ -0,0 +1,36 @@
+{
+	//   ## 📚 3.2: Inheritance in OOP
+
+	// 1. **What is inheritance in OOP? Explain with an example (e.g., Teacher–Student or Father–Son).**
+	// 🔹 ** OOP-এ Inheritance কী? একটি উদাহরণসহ ব্যাখ্যা করুন (যেমন: শিক্ষক–শিক্ষার্থী, বাবা–ছেলে)।**
+
+	class Common {
+		constructor(public name: string, public address: string) {
+			this.name = name;
+			this.address = address;
+		}
+
+		exercise() {
+			return `${this.name} is doing physical activities`;
+		}
+	}
+
+	class Student extends Common {
+		constructor(public name: string, public address: string) {
+			super(name, address);
+		}
+	}
+
+	class Teacher extends Common {
+		constructor(public name: string, public address: string, public title: string) {
+			super(name, address);
+			this.title = title;
+		}
+	}
+
+	const teacher = new Teacher("Kamal Ahmed", "Nagarbari, tangail", "Professor");
+	const student = new Student("Jamal Ahmed", "Maloti, tangail");
+
+	console.log(teacher.exercise());
+	console.log(student.exercise());
+}
